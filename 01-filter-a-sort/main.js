f9370fc7c101e3d0d28c9c02d239d67440dd410c
Luděk Roleček <ludek@rolecek.cz>
2024-04-03 18:00:48 +0200

    Aktualizace podkladů

---
+++ 01-filter-a-sort/main.js
@@ -0,0 +1,55 @@
+console.log('Filter a sort')
+
+
+const knihy = [
+  {
+    nazev: "Sto roků samoty",
+    rokVydani: 1967,
+    autor: "Gabriel García Márquez",
+  },
+  {
+    nazev: "Pán prstenů: Společenstvo Prstenu",
+    rokVydani: 1954,
+    autor: "J. R. R. Tolkien",
+  },
+  {
+    nazev: "Malý princ",
+    rokVydani: 1943,
+    autor: "Antoine de Saint-Exupéry",
+  },
+  {
+    nazev: "Stopařův průvodce po Galaxii",
+    rokVydani: 1979,
+    autor: "Douglas Adams",
+  },
+  {
+    nazev: "1984",
+    rokVydani: 1949,
+    autor: "George Orwell",
+  },
+  {
+    nazev: "Zločin a trest",
+    rokVydani: 1866,
+    autor: "Fjodor Michajlovič Dostojevskij",
+  },
+  {
+    nazev: "Moby Dick",
+    rokVydani: 1851,
+    autor: "Herman Melville",
+  },
+  {
+    nazev: "Vojna a mír",
+    rokVydani: 1869,
+    autor: "Lev Nikolajevič Tolstoj",
+  },
+  {
+    nazev: "Faust",
+    rokVydani: 1808,
+    autor: "Johann Wolfgang von Goethe",
+  },
+  {
+    nazev: "Hamlet",
+    rokVydani: 1603,
+    autor: "William Shakespeare",
+  },
+];
